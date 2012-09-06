2d43947fe2a80372e5cb4fa334e58df7b54c35df
Dave Woodall <dmwoodall@gmail.com>
2012-09-06 13:23:54 -0600

    Stop. Haml time.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ CodeJournalApp::Application.routes.draw do
 
 resources :questions
 
-root to: 'questions#index'
+get 'index2' => 'questions#index2'
+
+get 'haml' => 'static#hamltest'
+get 'hello' => 'static#hello'
+
+
+root to: 'static#index'
 
 end
